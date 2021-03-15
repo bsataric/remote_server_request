d886f167e0359ab8103382bbc6d228be11fe49cb
Bogdan Sataric <bogdan.sataric@cern.ch>
2021-03-15 14:15:59 +0100

    Created dummy remote server request

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    {{ output }}
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Component, Vue, Watch } from 'vue-property-decorator'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import axios from 'axios'
 
 @Component({
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  @Watch('events')
+  onEventsReceived(val: any, oldVal: any) {
+    this.output = val
+    //console.log('OUTPUT: ' + this.output)
+  }
+  private events: any = null
+  private output: any = null
+
+  created() {
+    axios
+      .get('http://localhost:3000/events')
+      .then((response) => {
+        //console.log('RESPONSE DATA: ' + JSON.stringify(response.data))
+        this.events = response.data
+      })
+      .catch((error) => {
+        console.log('There was an error: ' + error.response)
+      })
+  }
+}
 </script>
